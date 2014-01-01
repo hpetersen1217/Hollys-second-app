9d45a31e6993a014e382b94621237ba6fbef25d1
HollyPetersen <h.petersen@live.com>
2014-01-01 14:05:11 -0800

    First commit and README update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HollysBlocipedia::Application.config.secret_token = 'bcf5d47f6c3738d9e4f5d91b0b44f819b8f5d8a488c9a8065491c3268683abf0c86b199caac2cfe09f55d73523fb1a43492aea1f1579801633c3d474904b286f'
